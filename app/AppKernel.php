f601884d58716dff38f08b3ed9664ca57ffc72ae
Rodrigo Prado de Jesus <royopa@gmail.com>
2014-07-22 21:23:06 -0300

    incluído percentual de rendimento na lista de posição

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Royopa\SicinBundle\RoyopaSicinBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Ddeboer\DataImportBundle\DdeboerDataImportBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
